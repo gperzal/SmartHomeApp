1453a066834f64c9ffdfedb64ee4f48274581515
Guido Perez <gperzal@gmail.com>
2025-06-11 21:48:03 -0400

    feat: :memo: add versión message to test pipeline :rocket:

--- src/main/java/com/smarthome/App.java
+++ src/main/java/com/smarthome/App.java
@@ -3,5 +3,6 @@ package com.smarthome;
 public class App {
     public static void main(String[] args) {
         System.out.println("SmartHomeApp funcionando correctamente ✅");
+        System.out.println("🏠 Versión 1.1 - Pipeline de prueba activado!");
     }
 }
